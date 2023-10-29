1f2b0d563725a6e1decbd0ee34338903f0574d8a
DeepeshKalura <deepeshkalurs@gmail.com>
2023-10-29 19:58:39 +0530

    create register screen

---
+++ lib/view/screen/register_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}

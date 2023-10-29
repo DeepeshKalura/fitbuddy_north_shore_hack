399220ed13c2d9856a8c008ec8fbe0d2f4d56b2c
DeepeshKalura <deepeshkalurs@gmail.com>
2023-10-29 20:02:57 +0530

    register screen path as routes

---
+++ lib/controller/route/routes_name.dart
@@ -0,0 +1,9 @@
+class RoutesName {
+  // There register screens
+  static const String registerScreen = '/registerScreen';
+  static const String registerCompleteScreen = '/registerCompleteScreen';
+  static const String registerGoalScreen = '/registerGoalScreen';
+
+  static const String loginScreen = '/loginScreen';
+  static const String homeScreen = '/homeScreen';
+}

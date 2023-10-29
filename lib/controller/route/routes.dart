f27c5e43fd0a382fcd2447aa697ebdccc29fe842
DeepeshKalura <deepeshkalurs@gmail.com>
2023-10-29 20:03:21 +0530

    routesetting of register screen

---
+++ lib/controller/route/routes.dart
@@ -0,0 +1,21 @@
+import 'package:fitbuddy_north_shore_hack/controller/route/routes_name.dart';
+import 'package:flutter/material.dart';
+
+import '../../view/screen/no_route_screen.dart';
+import '../../view/screen/register_screen.dart';
+
+class Routes {
+  Route<dynamic> onGenerating(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RoutesName.registerScreen:
+        return MaterialPageRoute<dynamic>(
+            builder: (_) => const RegisterScreen());
+      default:
+        return MaterialPageRoute<dynamic>(
+          builder: (_) => NoRoutesScreen(
+            routeName: routeSettings.name ?? "No Route",
+          ),
+        );
+    }
+  }
+}

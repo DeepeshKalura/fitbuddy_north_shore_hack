405e9865a89530d1837f278b9b06e654c752b296
DeepeshKalura <deepeshkalurs@gmail.com>
2023-10-31 15:01:56 +0530

    started view

---
+++ lib/view/screen/on_boarding/started_view.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import '../../global/color_extension.dart';
+import '../../global/widget/round_button_widget.dart';
+import 'on_boarding_view.dart';
+
+class StartedView extends StatefulWidget {
+  const StartedView({super.key});
+
+  @override
+  State<StartedView> createState() => _StartedViewState();
+}
+
+class _StartedViewState extends State<StartedView> {
+  bool isChangeColor = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: TColor.white,
+      body: Container(
+          width: media.width,
+          decoration: BoxDecoration(
+            gradient: isChangeColor
+                ? LinearGradient(
+                    colors: TColor.primaryG,
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)
+                : null,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Text(
+                "Fitness",
+                style: TextStyle(
+                    color: TColor.black,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w700),
+              ),
+              Text(
+                "Everybody Can Train",
+                style: TextStyle(
+                  color: TColor.gray,
+                  fontSize: 18,
+                ),
+              ),
+              const Spacer(),
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: RoundButton(
+                    title: "Get Started",
+                    type: isChangeColor
+                        ? RoundButtonType.textGradient
+                        : RoundButtonType.bgGradient,
+                    onPressed: () {
+                      if (isChangeColor) {
+                        //GO Next Screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const OnBoardingView(),
+                          ),
+                        );
+                      } else {
+                        //Change Color
+                        setState(() {
+                          isChangeColor = true;
+                        });
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}

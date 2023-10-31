d904d6dc65ac9598a5fda03cc8720db8ee6226df
DeepeshKalura <deepeshkalurs@gmail.com>
2023-10-31 15:05:48 +0530

    refactor the route register screen was removed

--- lib/controller/route/routes.dart
+++ lib/controller/route/routes.dart
@@ -1,16 +1,10 @@
-import 'package:fitbuddy_north_shore_hack/controller/route/routes_name.dart';
 import 'package:flutter/material.dart';
 
 import '../../view/screen/no_route_screen.dart';
-import '../../view/screen/register_screen.dart';
 
 class Routes {
   static Route<dynamic> onGenerating(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case RoutesName.registerScreen:
-        return MaterialPageRoute<dynamic>(
-          builder: (_) => const RegisterScreen(),
-        );
       default:
         return MaterialPageRoute<dynamic>(
           builder: (_) => NoRoutesScreen(
